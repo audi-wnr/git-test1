cbe817bbabf75822d94dcd61a15d7e40d17e128b
zhouwj <zhouwj@icarplus.net>
2020-06-21 15:10:38 +0800

    提交demo1

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,4 +2,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("打印demo1");
     }
+    public void test(){
+        int a=10;
+        int b=20;
+    }
 }
